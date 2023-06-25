2800eb5f8b80db2065b710ff2d78c498059ee73a
l0stKitten <92339980+l0stKitten@users.noreply.github.com>
2023-06-25 03:50:16 -0500

    Add files via upload

--- FilmRecommend.py
+++ FilmRecommend.py
@@ -1,44 +1,66 @@
 from pyspark.sql import SparkSession
 from pyspark.ml.recommendation import ALS
+from pyspark.ml.feature import StringIndexer
 from pyspark.sql.functions import col
 
-# Create a SparkSession
-spark = SparkSession.builder \
-    .appName("FIlmRecommendation") \
-    .getOrCreate()
+# Create a Spark session
+spark = SparkSession.builder.appName("Film Recommender").getOrCreate()
 
-# Load the user-song listening history data
-film_history = spark.read.csv("MovieUserRating.csv", header=True, inferSchema=True,  sep = ',')
+# Set the options for reading the CSV file
+csv_options = {
+    "header": "true",
+    "encoding": "utf-8",
+    "sep": ","
+}
 
-# Prepare the data for training
-film_history = listening_history.select("userId", "movieId", "rating")
+# Load the MovieLens dataset
+data_path = "MovieUserRating.csv"
+df = spark.read.csv("MovieUserRating.csv", header=True, inferSchema=True,  sep = ',')
 
-# Split the data into training and testing sets
-(training_data, testing_data) = film_history.randomSplit([0.8, 0.2])
+# Select relevant columns and rename them
+df = df.select("userId", "movieId", "rating").withColumnRenamed("userId", "user").withColumnRenamed("movieId", "movie")
 
-# Build the recommendation model using ALS
-als = ALS(userCol="userId", movieCol="movieId", ratingCol="rating", coldStartStrategy="drop")
-model = als.fit(training_data)
+# Convert rating column to float
+df = df.withColumn("rating", df["rating"].cast("float"))
 
-# Generate top 10 recommendations for each user in the testing set
-recommendations = model.recommendForAllUsers(10)
+# Filter out any invalid or missing values
+df = df.filter(df["user"].isNotNull() & df["rating"].isNotNull() & df["movie"].isNotNull())
 
-# Show the recommendations for a specific user
-user_id = 1
-user_recommendations = recommendations.filter(recommendations.user_id == user_id).select("recommendations")
-user_recommendations.show(truncate=False)
+
+# Split the data into training and testing sets (80% for training, 20% for testing)
+train_data, test_data = df.randomSplit([0.8, 0.2], seed=42)
+
+# Create StringIndexers for the user and song columns
+user_indexer = StringIndexer(inputCol="user", outputCol="userIndex")
+movie_indexer = StringIndexer(inputCol="movie", outputCol="movieIndex")
+
+# Fit StringIndexers and transform the data
+indexed_data = user_indexer.fit(train_data).transform(train_data)
+indexed_data = movie_indexer.fit(indexed_data).transform(indexed_data)
+
+# Create an ALS recommender model
+als = ALS(userCol="userIndex", itemCol="movieIndex", ratingCol="rating", nonnegative=True)
+
+# Fit the model to the training data
+model = als.fit(indexed_data)
+
+user_indexer = StringIndexer(inputCol="user", outputCol="userIndex")
+movie_indexer = StringIndexer(inputCol="movie", outputCol="movieIndex")
+
+# Fit StringIndexers and transform the data
+indexed_test_data = user_indexer.fit(test_data).transform(test_data)
+indexed_test_data = movie_indexer.fit(indexed_test_data).transform(indexed_test_data)
 
 id_to_retrieve = 1
 
 filtered_data = indexed_data.filter(col("movieIndex") == id_to_retrieve)
 
 movie_index = filtered_data.select("movie").collect()[0][0]
+
 print(movie_index)
 
 df_original = spark.read.format("csv").options(**csv_options).load(data_path)
 
-artist_name = (df_original.filter(col("movieId") == movie_index)).select("title").collect()[0][0]
-print(artist_name)
+film_name = (df_original.filter(col("movieId") == movie_index)).select("title").collect()[0][0]
 
-genre_name = (df_original.filter(col("movieId") == movie_index)).select("genres").collect()[0][0]
-print(genre_name)
+print(film_name)
